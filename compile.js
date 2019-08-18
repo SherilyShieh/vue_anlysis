4c637216e8ac0879e63e398beb519416fcfe3ac1
SherilyShieh <1252524051@qq.com>
2019-08-18 12:28:38 +0800

    feat: 指定

--- compile.js
+++ compile.js
@@ -35,7 +35,25 @@ class Compile {
             // 类型判断
             if (this.isElement(node)) {
                 // 元素
-                // console.log('编译元素' + node.nodeName);
+                console.log('编译元素' + node.nodeName);
+                // 查找k-,@,:
+                const nodeAttrs = node.attributes;
+                Array.from(nodeAttrs).forEach(attr => {
+                    console.log('解析元素', attr)
+                    const attrName = attr.name; // 属性名
+                    const exp = attr.value; // 属性值
+                    if (this.isDirective(attrName)) {
+                        console.log('这是一行指令', attrName)
+                            // k-text
+                        const dir = attrName.substring(2);
+                        // 执行指令
+                        this[dir] && this[dir](node, this.$vm, exp)
+                    }
+                    if (this.isEvent(attrName)) {
+                        const dir = attrName.substring(1); // @click
+                        this.eventHandler(node, this.$vm, exp, dir)
+                    }
+                })
 
             } else if (this.isInterpolation(node)) {
                 // 文本
@@ -64,12 +82,48 @@ class Compile {
             updaterFn && updaterFn(node, value);
         })
     }
-
-    textUpdater(node, value) {
-        node.textContent = value;
-
+    text(node, vm, exp) {
+            console.log('执行指令')
+            this.update(node, vm, exp, 'text')
+        }
+        // 双向绑定
+    model(node, vm, exp) {
+        // 指定input的value属性
+        this.update(node, vm, exp, "model");
+        //视图对模型的响应
+        node.addEventListener("input", e => {
+            vm[exp] = e.target.value;
+        })
     }
 
+    modelUpdater(node, value) {
+        node.value = value;
+    }
+
+    html(node, vm, exp) {
+        this.update(node, vm, exp, "html");
+
+    }
+    htmlUpdater(node, value) {
+        node.innerHTML = value;
+    }
+
+    textUpdater(node, value) {
+            node.textContent = value;
+        }
+        // 事件处理器
+    eventHandler(node, vm, exp, dir) {
+        const fn = vm.$options.methods && vm.$options.methods[exp];
+        if (dir && fn) {
+            node.addEventListener(dir, fn.bind(vm));
+        }
+    }
+    isDirective(attr) {
+        return attr.indexOf('k-') == 0;
+    }
+    isEvent(attr) {
+        return attr.indexOf('@') == 0;
+    }
     isElement(node) {
         return node.nodeType === 1;
     }
